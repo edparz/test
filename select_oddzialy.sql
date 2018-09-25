ba5c6968f29c4b3cd8d0c811b7996b581eecd1c1
ed_parz <ed_parz@op.pl>
2018-09-25 15:39:58 +0200

    2 nowe pliki, 3 zmienione

--- select_oddzialy.sql
+++ select_oddzialy.sql
@@ -1,15 +1,15 @@
 -- nagłówek -----------------------
 DECLARE
 
-l_zmienna int := 223;
+  l_zmienna int := 223;
 
 BEGIN
 
    FOR l_rekord IN
    (
      SELECT IDDZIALU,NAZWA FROM DZIALY
-     WHERE IDDZIALU > l_zmienna
-     ORDER BY NAZWA
+      WHERE IDDZIALU > l_zmienna
+      ORDER BY NAZWA
    )
    LOOP
      dbms_output.put_line('Nazwa działu ' || l_rekord.nazwa);
--- update_oddzialy.sql
+++ update_oddzialy.sql
@@ -8,9 +8,11 @@ declare
 begin
 	--pobranie danych zapytaniem SQL do typu wierszowego
 	select D.* into L_dzial_row from DZIALY D where D.IDDZIALU = L_id_dzialu;
+	
 	--modyfikacja zapytaniem SQL z typem wierszowym
 	L_dzial_row.NAZWA := 'Nowa nazwa';
 	update DZIALY D set row = L_dzial_row where D.IDDZIALU = L_id_dzialu;
+	
 	--SQL insert z typem wierszowym
 	L_dzial_row.IDDZIALU := -999;
 	insert into DZIALY values L_dzial_row;
